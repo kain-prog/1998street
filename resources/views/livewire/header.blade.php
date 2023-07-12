916922246f2d5ecda1bef92842c387d5554a8060
Kain <mtheusmss@gmail.com>
2023-07-12 00:06:06 -0300

    feature: edit header component

---
+++ resources/views/livewire/header.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg" style="background: #010101; border-bottom:1px solid #ffffff1f">
+    <div class="container w-100 d-flex align-items-center justify-content-between" >
+        <div class="col-2">
+
+        </div>
+
+        <h1 class="col-8 d-flex align-items-center justify-content-center">
+            <a href="#">
+                <img src="/imgs/logo.png" alt="logo 1999Street" style="width: 165px;">
+            </a>
+        </h1>
+
+        <div class="col-2 d-flex align-items-center justify-content-center">
+            <div class="logged d-flex align-items-center justify-content-center ">
+                <a class="text-decoration-none" href="#" style="color: #ffffffdb;">Entrar</a>
+                <span class="mx-4" style="color: #ffffffdb;">|</span>
+                <a class="text-decoration-none" href="#" style="color: #ffffffdb;">Cadastrar</a>
+            </div>
+            {{-- <i class="fa-regular fa-bell"></i> --}}
+        </div>
+    </div>
+</nav>
+
+<nav class="navbar navbar-expand-lg" style="background: #010101;">
+    <div class="container d-flex align-items-center justify-content-center">
+        <ul class="nav">
+            <li class="nav-item"><a class="nav-link" href="" style="font-size: 13px; color: #ffffffdb;">Todos os Produtos</a></li>
+            <li class="nav-item"><a class="nav-link" href="" style="font-size: 13px; color: #ffffffdb;">Categorias</a></li>
+            <li class="nav-item"><a class="nav-link" href="" style="font-size: 13px; color: #ffffffdb;">Mais Vendidos</a></li>
+            <li class="nav-item"><a class="nav-link" href="" style="font-size: 13px; color: #ffffffdb;">Descontos</a></li>
+        </ul>
+    </div>
+</nav>
