cae40bf3250870b591eb2e1e75d3fd13ff7a1c67
Kain <mtheusmss@gmail.com>
2023-07-12 00:05:17 -0300

    feature: create banner component livewire

---
+++ resources/views/livewire/banner.blade.php
@@ -0,0 +1,32 @@
+<div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="d-flex w-100" style="height: 550px">
+            <img class="" src="/imgs/gallery-white/pxllera1.jpeg" alt="First slide" style="width: 33.3%">
+            <img class="" src="/imgs/gallery-white/pxllera2.jpg" alt="" style="width: 33.3%">
+            <img class="" src="/imgs/gallery-white/pxllera3.jpg" alt="" style="width: 33.3%">
+            <div class="gradient-overlay"></div>
+
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="d-flex w-100" style="height: 550px">
+            <img class="" src="/imgs/gallery-black/pxllerab-3.jpg" alt="First slide" style="width: 33.3%">
+            <img class="" src="/imgs/gallery-black/pxllerab-2.jpg" alt="" style="width: 33.3%">
+            <img class="" src="/imgs/gallery-black/pxllerab-1.jpg" alt="" style="width: 33.3%">
+            <div class="gradient-overlay"></div>
+        </div>
+      </div>
+      {{-- <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+      </div> --}}
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+</div>
