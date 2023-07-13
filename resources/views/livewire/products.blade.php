81fbb7958d00160128fb6d40277f5651b5d60772
Kain <mtheusmss@gmail.com>
2023-07-13 00:03:44 -0300

    feature: create product component

---
+++ resources/views/livewire/products.blade.php
@@ -0,0 +1,22 @@
+<div id="products" class="container position-relative">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-header">
+                    <img src="/imgs/" alt="">
+                </div>
+
+                <div class="card-body">
+                    teste
+                </div>
+
+            </div>
+        </div>
+        <div class="col-sm-4">
+
+        </div>
+        <div class="col-sm-4">
+
+        </div>
+    </div>
+</div>
