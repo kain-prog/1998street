97d6d71fac6e348a4d34e867f2bca954e19dcc87
Kain <mtheusmss@gmail.com>
2023-07-15 14:02:14 -0300

    feature: insert product css and component

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -1,22 +1,263 @@
 <div id="products" class="container position-relative">
     <div class="row">
-        <div class="col-sm-4">
-            <div class="card">
-                <div class="card-header">
-                    <img src="/imgs/" alt="">
+
+        <div class="col-md-4">
+            <div class="card mb-5 shadow">
+
+                <div class="card-header d-flex align-items-center justify-content-between">
+                    <h2 class="name-product">1998Street Superior</h2>
+                    <i class="fa-regular fa-heart"></i>
                 </div>
 
-                <div class="card-body">
-                    teste
+                <div class="card-body d-flex justify-content-center align-items-center position-relative" style="">
+                    <img class="img-thumbnail" src="/imgs/products/1998-white.jpeg" alt="">
+                    <a class="favorite-product" href="#">
+                        <i class="text-danger position-absolute fa-regular fa-heart"></i>
+                    </a>
+                </div>
+
+                <div class="card-footer">
+                    <div class="product-info row">
+                        <div class="col-lg-8 desc">
+                            
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ad distinctio quia alias saepe enim.</p>
+                        </div>
+
+                        <div class="col-lg-4 price align-items-center">
+                            <p class="text-center m-0 mb-1">R$ <span class="position-relative price-discount">70,00</span></p>
+                            <h3 class="text-center">R$ 59,99</h3>
+                        </div>
+
+                    </div>
+                    <div class="btn-container row p-2">
+
+                        <a class="btn btn-dark mb-2 py-3" href="">
+                            <i class="fa-solid fa-cart-arrow-down me-3"></i>
+                            <span>Adicionar no carrinho</span>
+                        </a>
+
+                        <a class="btn btn-success" href="">
+                            <i class="fa-solid fa-circle-info me-3"></i>
+                            <span>Ver produto</span>
+                        </a>
+                    </div>
                 </div>
 
             </div>
         </div>
-        <div class="col-sm-4">
+        <div class="col-md-4">
+            <div class="card mb-5 shadow">
 
+                <div class="card-header d-flex align-items-center justify-content-between">
+                    <h2 class="name-product">1998Street Superior</h2>
+                    <i class="fa-regular fa-heart"></i>
+                </div>
+
+                <div class="card-body d-flex justify-content-center align-items-center" style="">
+                    <img class="img-thumbnail" style="" src="/imgs/products/1998-black.jpeg" alt="">
+                </div>
+
+                <div class="card-footer">
+
+                    <div class="product-info row">
+
+                        <div class="col-lg-8 desc">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ad distinctio quia alias saepe enim.</p>
+                        </div>
+
+                        <div class="col-lg-4 price align-items-center">
+                            <p class="text-center m-0 mb-1">R$<span class="position-relative price-discount">70,00</span></p>
+                            <h3 class="text-center">R$ 59,99</h3>
+                        </div>
+
+                    </div>
+
+                    <div class="btn-container row p-2">
+
+                        <a class="btn btn-dark mb-2 py-3" href="">
+                            <i class="fa-solid fa-cart-arrow-down me-3"></i>
+                            <span>Adicionar no carrinho</span>
+                        </a>
+
+                        <a class="btn btn-success" href="">
+                            <i class="fa-solid fa-circle-info me-3"></i>
+                            <span>Ver produto</span>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
         </div>
-        <div class="col-sm-4">
+        <div class="col-md-4">
+            <div class="card mb-5 shadow">
 
+                <div class="card-header d-flex align-items-center justify-content-between">
+                    <h2 class="name-product">1998Street Superior</h2>
+                    <i class="fa-regular fa-heart"></i>
+                </div>
+
+                <div class="card-body d-flex justify-content-center align-items-center" style="">
+                    <img class="img-thumbnail" src="/imgs/products/1998-white.jpeg" alt="" >
+                </div>
+
+                <div class="card-footer">
+                    <div class="product-info row">
+                        <div class="col-lg-8 desc">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ad distinctio quia alias saepe enim.</p>
+                        </div>
+
+                        <div class="col-lg-4 price align-items-center">
+                            <p class="text-center m-0 mb-1">R$<span class="position-relative price-discount">70,00</span></p>
+                            <h3 class="text-center">R$ 59,99</h3>
+                        </div>
+
+                    </div>
+
+                    <div class="btn-container row p-2">
+
+                        <a class="btn btn-dark mb-2 py-3" href="">
+                            <i class="fa-solid fa-cart-arrow-down me-3"></i>
+                            <span>Adicionar no carrinho</span>
+                        </a>
+
+                        <a class="btn btn-success" href="">
+                            <i class="fa-solid fa-circle-info me-3"></i>
+                            <span>Ver produto</span>
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card mb-5 shadow">
+
+                <div class="card-header d-flex align-items-center justify-content-between">
+                    <h2 class="name-product">1998Street Superior</h2>
+                    <i class="fa-regular fa-heart"></i>
+                </div>
+
+                <div class="card-body d-flex justify-content-center align-items-center" style="">
+                    <img class="img-thumbnail" src="/imgs/products/1998-white.jpeg" alt="">
+                </div>
+
+                <div class="card-footer">
+                    <div class="product-info row">
+                        <div class="col-lg-8 desc">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ad distinctio quia alias saepe enim.</p>
+                        </div>
+
+                        <div class="col-lg-4 price align-items-center">
+                            <p class="text-center m-0 mb-1">R$<span class="position-relative price-discount">70,00</span></p>
+                            <h3 class="text-center">R$ 59,99</h3>
+                        </div>
+
+                    </div>
+
+                    <div class="btn-container row p-2">
+
+                        <a class="btn btn-dark mb-2 py-3" href="">
+                            <i class="fa-solid fa-cart-arrow-down me-3"></i>
+                            <span>Adicionar no carrinho</span>
+                        </a>
+
+                        <a class="btn btn-success" href="">
+                            <i class="fa-solid fa-circle-info me-3"></i>
+                            <span>Ver produto</span>
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card mb-5 shadow">
+
+                <div class="card-header d-flex align-items-center justify-content-between">
+                    <h2 class="name-product">1998Street Superior</h2>
+                    <i class="fa-regular fa-heart"></i>
+                </div>
+
+                <div class="card-body d-flex justify-content-center align-items-center">
+                    <img class="img-thumbnail" src="/imgs/products/1998-white.jpeg" alt="">
+                </div>
+
+                <div class="card-footer">
+                    <div class="product-info row">
+                        <div class="col-lg-8 desc">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ad distinctio quia alias saepe enim.</p>
+                        </div>
+
+                        <div class="col-lg-4 price align-items-center">
+                            <p class="text-center m-0 mb-1">R$<span class="position-relative price-discount">70,00</span></p>
+                            <h3 class="text-center">R$ 59,99</h3>
+                        </div>
+
+                    </div>
+
+                    <div class="btn-container row p-2">
+
+                        <a class="btn btn-dark mb-2 py-3" href="">
+                            <i class="fa-solid fa-cart-arrow-down me-3"></i>
+                            <span>Adicionar no carrinho</span>
+                        </a>
+
+                        <a class="btn btn-success" href="">
+                            <i class="fa-solid fa-circle-info me-3"></i>
+                            <span>Ver produto</span>
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card mb-5 shadow">
+
+                <div class="card-header d-flex align-items-center justify-content-between">
+                    <h2 class="name-product">1998Street Superior</h2>
+                    <i class="fa-regular fa-heart"></i>
+                </div>
+
+                <div class="card-body d-flex justify-content-center align-items-center">
+                    <img class="img-thumbnail" src="/imgs/products/1998-white.jpeg" alt="">
+                </div>
+
+                <div class="card-footer">
+                    <div class="product-info row">
+                        <div class="col-lg-8 desc">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ad distinctio quia alias saepe enim.</p>
+                        </div>
+
+                        <div class="col-lg-4 price align-items-center">
+                            <p class="text-center m-0 mb-1">R$<span class="position-relative price-discount">70,00</span></p>
+                            <h3 class="text-center">R$ 59,99</h3>
+                        </div>
+
+                    </div>
+
+                    <div class="btn-container row p-2">
+
+                        <a class="btn btn-dark mb-2 py-3" href="">
+                            <i class="fa-solid fa-cart-arrow-down me-3"></i>
+                            <span>Adicionar no carrinho</span>
+                        </a>
+
+                        <a class="btn btn-success" href="">
+                            <i class="fa-solid fa-circle-info me-3"></i>
+                            <span>Ver produto</span>
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
